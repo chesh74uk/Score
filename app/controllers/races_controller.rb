a415f8a001febae5845cc088be067ebfca381b90
Chesh <chesh@chesh.info>
2018-05-25 20:19:27 +0000

    added button to meeting page

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -8,8 +8,26 @@ class RacesController < ApplicationController
         @race = Race.find(params[:id])
     end
     
+    def edit
+        @race = Race.find(params[:id])
+    end
+    
+    def update
+        @race = Race.find(params[:id])
+        @race.update(races_params)
+        redirect_to(meeting_path(@race.meeting_id))
+    end
+    
+    def destroy
+      @race = Race.find(params[:id])
+      meeting_id = @race.meeting_id
+      @race.destroy
+      redirect_to(meeting_path(@race.meeting_id))
+    end
+    
     def create
-        @race = Race.create(races_params)
+        @meeting = Meeting.find(params[:id])
+        @race = @meeting.race.build(races_params)
         if @race.save
             flash[:success] = "Race created"
             redirect_to @race
