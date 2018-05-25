867b3e1afe6d85af2449dd0fdfd9b9dcb19f65cd
Chesh <chesh@chesh.info>
2018-05-25 14:32:25 +0000

    created list of meetings on index page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,7 @@
 class MeetingsController < ApplicationController
     
     def index
+        @meetings = Meeting.all
     end
     
     def new
--- spec/features/index_displays_list_of_meetings_spec.rb
+++ spec/features/index_displays_list_of_meetings_spec.rb
@@ -5,5 +5,7 @@ feature "Displays a list of meetings" do
         meeting_one = create(:meeting, meeting_date: "23/05/2018", home_team: "Belle Vue", away_team: "Swindon")
         meeting_two = create(:meeting, meeting_date: "07/06/2018", home_team: "Kings Lynn", away_team: "Poole")
         visit '/'
+        expect(page).to have_content("Belle Vue")
+        expect(page).to have_content("Poole")
     end
 end
