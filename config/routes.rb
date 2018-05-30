39321182de4a87e388c454e56d628c3d521b6beb
Chesh <chesh@chesh.info>
2018-05-30 15:35:23 +0000

    added nested resources for home & away riders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'meetings#index'
-  resources :meetings
+  resources :meetings do
+    resources :home_riders
+    resources :away_riders
+  end
+  
   resources :races
 end
