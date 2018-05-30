5af507ecaa54afbe4bece3c8755c9c6def4851ba
Chesh <chesh@chesh.info>
2018-05-30 13:58:52 +0000

    accept home rider name to meeting form

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
     validates :home_team, :away_team, presence: true
     has_many :races, dependent: :destroy
-    has_many :home_riders, dependent: :destroy
+    has_many :home_riders, inverse_of: :meeting, dependent: :destroy
     accepts_nested_attributes_for :home_riders
 end
