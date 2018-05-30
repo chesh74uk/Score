46a298c90bfc16856b5efdf692bc078aff76e261
Chesh <chesh@chesh.info>
2018-05-30 13:49:07 +0000

    accept nested for home riders in meetings model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ApplicationRecord
     validates :home_team, :away_team, presence: true
     has_many :races, dependent: :destroy
+    has_many :home_riders, dependent: :destroy
+    accepts_nested_attributes_for :home_riders
 end
